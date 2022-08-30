55806f3bd0131f9227d7be20a40a7d411d5fe363
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-08-30 08:03:05 +0200

    Initial Commit

---
+++ GeometryReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryReader
+//
+//  Created by M. A. Alim Mukul on 30.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by M. A. Alim Mukul on 30.08.22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
